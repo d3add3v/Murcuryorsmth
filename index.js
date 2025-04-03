7c9c22d2cfdb9f40281272a89ad41255bd0f3fd8
Dead dev <c7474825@gmail.com>
2025-04-02 21:13:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-javascript:(function () {var a = document.createElement('script');a.src = 'https://www.mercuryworld.ml/js/script.js';document.body.appendChild(a);}())
+(function () {var a = document.createElement('script');a.src = 'https://www.mercuryworld.ml/js/script.js';document.body.appendChild(a);}())
